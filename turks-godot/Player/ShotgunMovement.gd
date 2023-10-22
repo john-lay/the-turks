96d870642a2c8e25ebbf49155cda66b6548db6b8
john-lay <john_lay@hotmail.co.uk>
2023-10-22 12:47:14 +0100

    Set up basic animated movement
    Clean up unused scripts

--- turks-godot/Player/ShotgunMovement.gd
+++ turks-godot/Player/ShotgunMovement.gd
@@ -2,25 +2,44 @@ extends KinematicBody2D
 
 var speed = 150  # speed in pixels/sec
 var velocity = Vector2.ZERO
-enum MOVEMENT {IDLE, LEFT, RIGHT, UP, DOWN}
-signal movement(MOVEMENT)
+enum MOVEMENT {LEFT, RIGHT, UP, DOWN}
+var direction = MOVEMENT.RIGHT 
 
+onready var animatedSprite = $AnimatedSprite
+
+func set_idle_direction():
+	if direction == MOVEMENT.RIGHT:
+		animatedSprite.animation = "idle-right"
+	if direction == MOVEMENT.LEFT:
+		animatedSprite.animation = "idle-left"
+	if direction == MOVEMENT.DOWN:
+		animatedSprite.animation = "idle-down"
+	if direction == MOVEMENT.UP:
+		animatedSprite.animation = "idle-up"
+		
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		emit_signal("movement", MOVEMENT.RIGHT)
+		animatedSprite.animation = "right"
+		direction = MOVEMENT.RIGHT
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		animatedSprite.animation = "left"
+		direction = MOVEMENT.LEFT
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		animatedSprite.animation = "down"
+		direction = MOVEMENT.DOWN
 	elif Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		animatedSprite.animation = "up"
+		direction = MOVEMENT.UP
 	else:
-		emit_signal("movement", MOVEMENT.IDLE)
-	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed
+		set_idle_direction()
+	
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
+	velocity = velocity.normalized() * speed
 	velocity = move_and_slide(velocity)
