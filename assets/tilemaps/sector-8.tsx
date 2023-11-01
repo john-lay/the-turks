178a4d04bcf77eb656b0f7428659bebe4d0f7d5e
john-lay <john_lay@hotmail.co.uk>
2023-11-01 18:03:03 +0000

    Add in battle UI components

--- assets/tilemaps/sector-8.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="sector-8" tilewidth="16" tileheight="16" tilecount="225" columns="15">
- <image source="../tilesets/sector-8.jpg" width="240" height="240"/>
-</tileset>
