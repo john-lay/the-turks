3902ec6dbed5fe0313487b3511bca9d31690cf64
john-lay <john_lay@hotmail.co.uk>
2024-03-25 16:05:43 +0000

    Add label to display damage

--- turks-godot/Enemy/avalanche.gd
+++ turks-godot/Enemy/avalanche.gd
@@ -4,11 +4,11 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var damageLabel = $DamageLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	damageLabel.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,4 +16,6 @@ func _ready():
 #	pass
 
 func enemy_hit():
+	damageLabel.text = "9999"
+	damageLabel.visible = true
 	print("enemy has been hit!")
