bf8578704ea55e325d6faaa80529acaa858a20ce
john-lay <john_lay@hotmail.co.uk>
2024-04-17 11:23:18 +0100

    Add a main game scene and dynamically load levels

---
+++ turks-godot/main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# https://bibo.sakura.ne.jp/ff7bc/data/character.html
+var player_level: int = 1
+var shotgun_hp: int = (60 * player_level) + 200
+var shotgun_mp: int = (7 * player_level) + 100
+var shotgun_attack_power: int = (2 * player_level) + 14
+var shotgun_defence_power: int = player_level + 14
+
+export(PackedScene) var BATTLE1: PackedScene = preload("res://Battle1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var battle = BATTLE1.instance()
+	get_tree().current_scene.add_child(battle)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
