236ab7bf188713cff7697c895ea0f7c0412164a6
john-lay <john_lay@hotmail.co.uk>
2024-06-11 15:00:09 +0100

    Fix magic cursor initial position bug

--- turks-godot/Magic/CastMagic.gd
+++ turks-godot/Magic/CastMagic.gd
@@ -53,7 +53,7 @@ func _get_input():
 
 func set_cursor_position(player_position: Vector2):
 	magic_cursor.visible = true
-	magic_cursor.position = initial_offset
+	magic_cursor.position = Vector2.ZERO
 	_set_cursor_position_x(player_position.x)
 	_set_cursor_position_y(player_position.y)
 	
