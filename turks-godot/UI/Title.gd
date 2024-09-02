1b6ddfed15d9664b8abcccfeb2e17123d530736f
john-lay <john_lay@hotmail.co.uk>
2024-09-02 21:39:09 +0100

    Allow the player to register their name

--- turks-godot/UI/Title.gd
+++ turks-godot/UI/Title.gd
@@ -5,6 +5,8 @@ signal select
 onready var menu_ui = $AnimatedSprite
 onready var select_audio = $SelectAudio
 onready var navigate_audio = $NavigateAudio
+onready var name_entry = $NameEntry
+onready var global = get_node("/root/Global")
 
 var current_selection = 1
 var manage_input: bool = false
@@ -43,8 +45,23 @@ func _get_input():
 			_set_selection()
 	if Input.is_action_just_pressed("ui_accept"):
 		manage_input = false
+		_set_player_name()
 		emit_signal("select", current_selection)
 		select_audio.play()
+
+
+func _get_default_player_name():
+	if current_selection == 1:
+		return "Ellen"
+	if current_selection == 2:
+		return "エレン"
+
+
+func _set_player_name():
+	if name_entry.text == "":
+		global.g_settings["player_name"] = _get_default_player_name()
+	else:
+		global.g_settings["player_name"] = name_entry.text
 
 
 func should_manage_input():
